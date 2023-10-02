c2dc227791b30130db312403a9c976a937337a02
Gabe0312 <121005678+Gabe0312@users.noreply.github.com>
2023-10-02 01:00:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".pancake-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  
+  
+  }
+11  
